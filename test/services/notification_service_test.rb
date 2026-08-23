457e1bf6ca92633bf5c1675b602da733524837b3
maplefoxgit <s223932052@deakin.edu.au>
2026-08-23 22:00:23 +1000

    test(notifications): cover preference gating

--- test/services/notification_service_test.rb
+++ test/services/notification_service_test.rb
@@ -83,11 +83,84 @@ class NotificationServiceTest < ActiveSupport::TestCase
     assert_equal 0, ActionMailer::Base.deliveries.count
   end
 
+  def test_feedback_notification_is_delivered_when_the_category_preference_is_on
+    user = FactoryBot.create(:user, receive_feedback_notifications: true)
+
+    assert_difference 'Notification.count', 1 do
+      notification = NotificationService.notify(
+        user: user, type: 'feedback', event: 'feedback_available', message: 'Feedback available.'
+      )
+
+      assert notification.persisted?
+    end
+
+    assert_equal 1, ActionMailer::Base.deliveries.count
+  end
+
+  def test_task_preference_gates_notifications_in_both_directions
+    user = FactoryBot.create(:user, receive_task_notifications: true)
+
+    assert_difference 'Notification.count', 1 do
+      notification = NotificationService.notify(
+        user: user, type: 'task', event: 'task_due_date_changed', message: 'Task date changed.'
+      )
+
+      assert notification.persisted?
+    end
+    assert_equal 1, ActionMailer::Base.deliveries.count
+
+    user.update!(receive_task_notifications: false)
+
+    assert_no_difference 'Notification.count' do
+      result = NotificationService.notify(
+        user: user, type: 'task', event: 'task_due_date_changed', message: 'Suppressed task change.'
+      )
+
+      assert_nil result
+    end
+    assert_equal 1, ActionMailer::Base.deliveries.count
+  end
+
+  def test_portfolio_preference_gates_notifications_in_both_directions
+    user = FactoryBot.create(:user, receive_portfolio_notifications: true)
+
+    assert_difference 'Notification.count', 1 do
+      notification = NotificationService.notify(
+        user: user, type: 'portfolio', event: 'portfolio_received', message: 'Portfolio received.'
+      )
+
+      assert notification.persisted?
+    end
+    assert_equal 1, ActionMailer::Base.deliveries.count
+
+    user.update!(receive_portfolio_notifications: false)
+
+    assert_no_difference 'Notification.count' do
+      result = NotificationService.notify(
+        user: user, type: 'portfolio', event: 'portfolio_received', message: 'Suppressed portfolio receipt.'
+      )
+
+      assert_nil result
+    end
+    assert_equal 1, ActionMailer::Base.deliveries.count
+  end
+
   def test_types_without_a_preference_are_always_delivered
     user = FactoryBot.create(:user, receive_task_notifications: false, receive_feedback_notifications: false, receive_portfolio_notifications: false)
 
     notification = NotificationService.notify(
       user: user, type: 'general', event: 'always_sent', message: 'General notice.'
+    )
+
+    assert notification.persisted?
+    assert_equal 1, ActionMailer::Base.deliveries.count
+  end
+
+  def test_extension_notifications_are_always_delivered
+    user = FactoryBot.create(:user, receive_task_notifications: false, receive_feedback_notifications: false, receive_portfolio_notifications: false)
+
+    notification = NotificationService.notify(
+      user: user, type: 'extension', event: 'extension_decided', message: 'Extension decision available.'
     )
 
     assert notification.persisted?
