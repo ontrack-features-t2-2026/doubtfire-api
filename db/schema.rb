93f8a72bc97d820fd0e84024ffe6b455a5c72025
Clupai8o0 <clupaio4@gmail.com>
2026-09-14 03:12:31 +1000

    chore(db): regenerate schema.rb for the processing options migration
    Dumped from a fresh MariaDB after loading the previous schema and running
    20260831000003.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_08_31_000002) do
+ActiveRecord::Schema[8.0].define(version: 2026_08_31_000003) do
   create_table "activity_types", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "abbreviation", null: false
@@ -766,6 +766,10 @@ ActiveRecord::Schema[8.0].define(version: 2026_08_31_000002) do
     t.datetime "submission_processing_finished_at"
     t.string "submission_processing_error_code"
     t.integer "submission_processing_attempts", default: 0, null: false
+    t.string "submission_processing_mode"
+    t.bigint "submission_processing_user_id"
+    t.boolean "submission_processing_test_submission", default: false, null: false
+    t.boolean "submission_processing_accepted_tii_eula", default: false, null: false
     t.index ["group_submission_id"], name: "index_tasks_on_group_submission_id"
     t.index ["project_id", "task_definition_id"], name: "tasks_uniq_proj_task_def", unique: true
     t.index ["project_id"], name: "index_tasks_on_project_id"
